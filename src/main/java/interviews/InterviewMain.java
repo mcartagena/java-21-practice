a50e305899cad139fbf0643530e45bc77159a9c8
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2026-04-27 16:48:10 -0400

    fea: new interview

---
+++ src/main/java/interviews/InterviewMain.java
@@ -0,0 +1,20 @@
+package interviews;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class InterviewMain {
+    public static void main(String[] args) {
+        // how many incidences are of each String
+        String[] arr = {"java","spring", "java","sql","Spring"};
+
+        Map<String, Long> result = Arrays.stream(arr)
+                .map(m -> m.toLowerCase())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println(result);
+
+    }
+}
